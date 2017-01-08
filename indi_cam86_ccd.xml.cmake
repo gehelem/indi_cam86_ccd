1776195326923ff7c7508ca46d9f60d936c396da
gehelem <gilles@joag.fr>
2017-01-08 15:55:03 +0100

    xml correction - again...

--- indi_cam86_ccd.xml.cmake
+++ indi_cam86_ccd.xml.cmake
@@ -2,7 +2,7 @@
 <driversList>
 <devGroup group="CCDs">
         <device label="Cam86 CCD" mdpd="true">
-                <driver name="Cam84 CCD">indi_cam86_ccd</driver>
+                <driver name="Cam86 CCD">indi_cam86_ccd</driver>
                 <version>@CAM86_VERSION_MAJOR@.@CAM86_VERSION_MINOR@</version>
 	</device>
 </devGroup>
